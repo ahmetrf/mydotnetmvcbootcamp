80bf13d7cb3fb55d43cd2b605158b9ab71779d22
ahmetrf <arifuu.rf@gmail.com>
2022-11-19 16:51:04 +0300

    Gün sonu.

--- day-01/ProductApp/Controllers/HomeController.cs
+++ day-01/ProductApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
 using ProductApp.Models;
 
@@ -6,26 +7,43 @@ namespace ProductApp.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
-        return View();
+        var list = new List<string>
+        {
+            "Ahmet",
+            "Arif",
+            "Ahmetoğlu"
+        };
+
+        return View("Index", list);
     }
 
     public IActionResult Privacy()
     {
         return View();
     }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    
+    public IActionResult GetProduct()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //var product = new Product();
+        //product.Id = 1;
+        //product.ProductName = "Computer";
+        //product.Price = 15000;
+
+        var product = new Product(2, "Laptop", 17000);
+
+        return View(product);
+    }
+
+    public IActionResult GetProducts()
+    {
+        var list = new List<Product>
+        {
+            new Product(1, "Ahmet", 123),
+            new Product(2, "Arif", 234),
+            new Product(3, "Ahmetoğlu", 345)
+        };
+        return View(list);
     }
 }
