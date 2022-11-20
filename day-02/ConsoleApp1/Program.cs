b7307f0610f51823b3abbcd04c0ac9a57aa9e90e
ahmetrf <arifuu.rf@gmail.com>
2022-11-20 10:20:27 +0300

    New Day

---
+++ day-02/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿var list = new List<string>();
+list.Add("Bilgisayar");
+list.Add("Telefon");
+list.Add("Table");
+
+foreach(var a in list)
+{
+    Console.WriteLine(a);
+}
+
+list.ForEach(aaa => Console.WriteLine(aaa));
+
+Console.ReadKey();
---
+++ day-02/ProductApp/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        var list = new List<string>
+        {
+            "Ahmet",
+            "Arif",
+            "Ahmetoğlu"
+        };
+
+        return View("Index", list);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    
+    public IActionResult GetProduct()
+    {
+        //var product = new Product();
+        //product.Id = 1;
+        //product.ProductName = "Computer";
+        //product.Price = 15000;
+
+        var product = new Product(2, "Laptop", 17000);
+
+        return View(product);
+    }
+
+    public IActionResult GetProducts()
+    {
+        var list = new List<Product>
+        {
+            new Product(1, "Ahmet", 123),
+            new Product(2, "Arif", 234),
+            new Product(3, "Ahmetoğlu", 345)
+        };
+        return View(list);
+    }
+}
