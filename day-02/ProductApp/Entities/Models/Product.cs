c5f4cd194fd1754d9d6e24f783d29ff4a1c58184
ahmetrf <arifuu.rf@gmail.com>
2022-11-20 15:41:23 +0300

    ...

--- day-02/ProductApp/Entities/Models/Product.cs
+++ day-02/ProductApp/Entities/Models/Product.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Repositories.EFCore
+namespace Entities.Models
 {
-    internal class RepositoryContent
+    internal class Product
     {
     }
 }
---
+++ day-02/ProductApp/Repositories/EFCore/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories.EFCore
+{
+    public class RepositoryContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public RepositoryContext(DbContextOptions<RepositoryContext> options)
+        {
+
+        }
+    }
+}
